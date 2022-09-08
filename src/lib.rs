05dd848936b59ea58d8d754e9c9a81fd0d8a6fc2
jongjin pak <belovian@gmail.com>
2022-09-08 17:39:25 +0900

    build and serve test is done.

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,18 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
 
 #[wasm_bindgen]
 pub fn entry_point() {
-    println!("Hello, world!");
+    console_log!("Hello, world!");
 }
